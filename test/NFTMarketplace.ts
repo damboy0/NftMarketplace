c919ce319ae18841618f8a6d9c656e20e287e68f
damboy0 <fagbolade006@gmail.com>
2024-09-20 19:15:57 +0100

    Updated Tests

--- test/NFTMarketplace.ts
+++ test/NFTMarketplace.ts
@@ -12,11 +12,36 @@ import {
         const [owner, buyer, creator] = await hre.ethers.getSigners();
         const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
         const marketplace = await NFTMarketplace.deploy();
-        await marketplace.deployed();
+        // await marketplace.deployed();
     
         return { marketplace, owner, buyer, creator };
       }
+      describe("Minting NFTs", function () {
+    
+        it("Should revert if price is 0", async function () {
+          const { marketplace, creator } = await loadFixture(deployNFTMarketplace);
+          await expect(
+            marketplace.connect(creator).mintNFT("ipfs://token-uri", 0)
+          ).to.be.revertedWith("Price must be greater than 0");
+        });
 
+
+        it("Should mint a new NFT and store the details", async function () {
+            const { marketplace, creator } = await loadFixture(deployNFTMarketplace);
+            const price = hre.ethers.parseUnits("1", "18");
+            const tokenURI = "ipfs://token-uri";
+
+            const tokenId = await marketplace.connect(creator).mintNFT(tokenURI, price);
+            const nft = await marketplace.nfts(tokenId);
+
+            console.log(nft.creator);
+            expect(nft.creator).to.equal(creator.address);
+            expect(nft.price).to.equal(price);
+            expect(nft.isListed).to.equal(true);
+    });
+    
+      });
+    
     
 
   });
